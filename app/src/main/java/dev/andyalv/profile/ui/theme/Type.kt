0f21dc4ccad9dccdc52cd587f33cd50879a22ab5
andyalv <andy.red02@gmail.com>
2025-12-16 06:11:46 -0600

    Define custom typography and fonts

--- app/src/main/java/dev/andyalv/profile/ui/theme/Type.kt
+++ app/src/main/java/dev/andyalv/profile/ui/theme/Type.kt
@@ -1,34 +1,43 @@
 package dev.andyalv.profile.ui.theme
 
 import androidx.compose.material3.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import dev.andyalv.profile.R
+
+val Pixelifysans = FontFamily(
+    Font(R.font.pixelifysans_regular, FontWeight.Normal),
+    Font(R.font.pixelifysans_medium, FontWeight.Medium),
+    Font(R.font.pixelifysans_bold, FontWeight.Bold),
+    Font(R.font.pixelifysans_semibold, FontWeight.SemiBold)
+)
+
+val RainyHearts = FontFamily(
+    Font(R.font.rainyhearts, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyLarge = Typography().bodyLarge.copy(
+        fontFamily = RainyHearts,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 28.sp,
+        letterSpacing = 1.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    titleLarge = Typography().titleLarge.copy(
+        fontFamily = Pixelifysans,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    headlineSmall = Typography().headlineSmall.copy(
+        fontFamily = Pixelifysans,
+        fontWeight = FontWeight.Bold
+    ),
+    titleMedium = Typography().titleMedium.copy(
+        fontFamily = RainyHearts,
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold
     )
-    */
 )
